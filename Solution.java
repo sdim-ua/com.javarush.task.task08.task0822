101579098209d2eb5bc58ab41c207bd88beceabd
sdim-ua <ingersol@yandex.ua>
2019-03-13 22:01:39 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task08.task0822;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Collections;
+
+
+/* 
+Минимальное из N чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> array) {
+        // Найти минимум тут
+        
+        int minList = Collections.min(array); 
+        return minList;
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+        // Создать и заполнить список тут
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());
+          
+     LinkedList <Integer> list = new LinkedList<Integer>();
+ 
+       for (int i = 0; i < N;i ++){
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+       
+        return list;
+    }
+}
